7a43e91d12f6c228ffc16604bd89bc10d256e170
emnurul <emilylim.cm@gmail.com>
2024-05-28 18:33:17 +1000

    no meta

---
+++ 14-sleep-nometa.php
@@ -0,0 +1,10 @@
+<?php
+// Delay the execution for 10 seconds
+sleep(10);
+?>
+<html lang="en">
+<body>
+    <h1>Welcome to the Delayed Load Page</h1>
+    <p>This content was loaded after a delay of 10 seconds.</p>
+</body>
+</html>
